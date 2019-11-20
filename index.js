4ff7d5ae7014b608e16fbfdc014adf9222287e1a
Lance <gohlance@gmail.com>
2019-11-20 14:27:01 +0800

    testing

--- index.js
+++ index.js
@@ -37,6 +37,43 @@ app.use(logger());
 const router = new koa_router()
 require('./routes/basic')({ router })
 require('./routes/sfdc')({router})
+
+var oauth2 = new jsforce.OAuth2({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  // loginUrl : 'https://test.salesforce.com',
+  clientId : '3MVG9i1HRpGLXp.qKwbWJHwmeMEDkgggAcpbAf1Y1O7YvezHR_7aOv00w2a_Vz3gst8vk23v4e3qfLRbkKsFi',
+  clientSecret : '5675F7043344E39EC5A402927491DA9040F7C857C7A6F0B4D0AF8D3AE69BA8DF',
+  redirectUri : 'https://testingauth123.herokuapp.com/auth3/login/return'
+});
+
+router
+.get('/oauth2/auth', function(request, response) {
+  Response.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }))
+})
+.get('oauth', '/auth3/login/return', function(req, res){
+    var conn = new jsforce.Connection({
+      oauth2: oauth2
+    })
+    var code = Request.param('code')
+    conn.authorize(code, function (err, userInfo) {
+        if (err) {
+          return console.error(err)
+        }
+        console.log("&&& : " + conn.accessToken)
+        console.log("&&& : " + conn.refreshToken)
+        console.log("&&& : " + conn.instanceUrl)
+        console.log("&&& : " + conn.userInfo.orgId)
+
+        global.accesscode = conn.accessToken
+        global.refreshToken = conn.refreshToken
+        global.profile = conn.instanceUrl
+        global.orgId = conn.userInfo.orgId
+        global.userinfo = conn.userInfo
+
+        res.send('success')
+      })
+  })
+
 app.use(router.routes()).use(router.allowedMethods)
 
 //port configuration
--- routes/basic.js
+++ routes/basic.js
@@ -1,13 +1,7 @@
 const passport = require('../auth')
 const jsforce = require('jsforce')
 
-var oauth2 = new jsforce.OAuth2({
-  // you can change loginUrl to connect to sandbox or prerelease env.
-  // loginUrl : 'https://test.salesforce.com',
-  clientId : '3MVG9i1HRpGLXp.qKwbWJHwmeMEDkgggAcpbAf1Y1O7YvezHR_7aOv00w2a_Vz3gst8vk23v4e3qfLRbkKsFi',
-  clientSecret : '5675F7043344E39EC5A402927491DA9040F7C857C7A6F0B4D0AF8D3AE69BA8DF',
-  redirectUri : 'https://testingauth123.herokuapp.com/auth3/login/return'
-});
+
 
 module.exports = ({
     router
@@ -19,32 +13,7 @@ module.exports = ({
       .get('hello', '/hello', (ctx) => {
         ctx.body = "Hello"
       })
-      .get('/oauth2/auth', function(req, res) {
-        res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }))
-      })
-      .get('oauth', '/auth3/login/return', function(req, res){
-          var conn = new jsforce.Connection({
-            oauth2: oauth2
-          })
-          var code = req.param('code')
-          conn.authorize(code, function (err, userInfo) {
-              if (err) {
-                return console.error(err)
-              }
-              console.log("&&& : " + conn.accessToken)
-              console.log("&&& : " + conn.refreshToken)
-              console.log("&&& : " + conn.instanceUrl)
-              console.log("&&& : " + conn.userInfo.orgId)
-
-              global.accesscode = conn.accessToken
-              global.refreshToken = conn.refreshToken
-              global.profile = conn.instanceUrl
-              global.orgId = conn.userInfo.orgId
-              global.userinfo = conn.userInfo
-
-              res.send('success')
-            })
-        })
+      
 
         //this is old section
         .get('auth3', '/auth3', (ctx) => {
