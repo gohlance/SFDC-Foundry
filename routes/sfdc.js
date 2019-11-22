7d6a8418e625fcd4d4b309c72aec4108917646cf
Lance Goh <gohlance@gmail.com>
2019-11-22 21:43:34 +0800

    extracting the class

--- routes/sfdc.js
+++ routes/sfdc.js
@@ -71,10 +71,12 @@ module.exports = ({
             console.log("*** Conn : " + conn.instanceUrl)
             console.log("*** Conn : " + conn.accessToken)
             console.log("Authenticated, stating call")
+          
             conn.sobject("Account").describe(function (err, meta) {
                 if (err) {
                     return console.error(err);
                 }
+                
                 console.log('meta : ' + meta)
                 console.log('Label : ' + meta.label);
                 console.log('Num of Fields : ' + meta.fields.length);
@@ -93,81 +95,14 @@ module.exports = ({
             var conn = new jsforce.Connection({
                 oauth2: oauth2,
                 instanceUrl: global.instanceUrl,
-                accessToken: global.accesscode,
-                version: 47
+                accessToken: global.accesscode
             })
+            var something = require('../util')
+            
+            something.sObjectDescribe(conn,"Account")
+            
+            something.meta(conn)
 
-            var types = [{
-                type: 'ApexTrigger',
-                folder: null
-            }]
-            conn.metadata.list(types, function (err, meta) {
-                if (err) {
-                    return console.error('err', err)
-                }
-                console.log(meta)
-            })
-            /*
-            var types = [{
-                type: 'CustomObject',
-                folder: null
-            }]
-            conn.metadata.list(types, function (err, meta) {
-                if (err) {
-                    return console.error('err', err)
-                }
-                console.log(meta)
-                console.log('metadata count: ' + metadata.length);
-                console.log('createdById: ' + meta.createdById);
-                console.log('createdByName: ' + meta.createdByName);
-                console.log('createdDate: ' + meta.createdDate);
-                console.log('fileName: ' + meta.fileName);
-                console.log('fullName: ' + meta.fullName);
-                console.log('id: ' + meta.id);
-                console.log('lastModifiedById: ' + meta.lastModifiedById);
-                console.log('lastModifiedByName: ' + meta.lastModifiedByName);
-                console.log('lastModifiedDate: ' + meta.lastModifiedDate);
-                console.log('manageableState: ' + meta.manageableState);
-                console.log('namespacePrefix: ' + meta.namespacePrefix);
-                console.log('type: ' + meta.type);
-            })*/
-
-            /*
-            //This give the information on the Objects
-            conn.describeGlobal(function (err,res){
-                if (err){return console.error(err)}
-                console.log('No of Objects ' + res)
-                var customObject =[]
-                var standardObject =[]
-                res.sobjects.forEach(function(sobject){
-                    if (sobject.custom){
-                        customObject.push(sobject)
-                    }else{
-                        standardObject.push(sobject)
-                    }
-                })
-                console.log("Standard object : " + standardObject.length)
-                console.log("Custom Object : " + customObject.length)
-            })*/
-            /*
-            //This get the fields in the Visualforce Page?
-            conn.tooling.sobject('ApexPage').describe(function (err, meta){
-                if (err){return console.error(err)}
-                console.log("label name : " + meta.label)
-                var i = 1
-                meta.fields.forEach(function(item){
-                    console.log(' *' + i + ' :' + item.label)
-                    i++
-                })
-            })
-            //This get the fields in the ApexTrigger
-            conn.tooling.describe('ApexTrigger', function(err, result){
-                if (err){return console.error(err)}
-                var i = 1
-                result.fields.forEach(function(item){
-                    console.log(i + ':' + item.label)
-                    i++
-                })
-            })*/
+            
         })
 }
---
+++ util.js
@@ -0,0 +1,98 @@
+function sObjectDescribe(conn, sobjectname){
+    conn.sobject(sobjectname).describe(function (err, meta) {
+        if (err) {
+            return console.error(err);
+        }
+        
+        console.log('meta : ' + meta)
+        console.log('Label : ' + meta.label);
+        console.log('Num of Fields : ' + meta.fields.length);
+        // ...
+    })
+}
+
+function meta(conn){
+    var types = [{
+        type: 'ApexTrigger',
+        folder: null
+    }]
+    conn.metadata.list(types, function (err, meta) {
+        if (err) {
+            return console.error('err', err)
+        }
+        console.log(meta)
+    })
+}
+
+function dofinally(conn){
+    /*
+            var types = [{
+                type: 'CustomObject',
+                folder: null
+            }]
+            conn.metadata.list(types, function (err, meta) {
+                if (err) {
+                    return console.error('err', err)
+                }
+                console.log(meta)
+                console.log('metadata count: ' + metadata.length);
+                console.log('createdById: ' + meta.createdById);
+                console.log('createdByName: ' + meta.createdByName);
+                console.log('createdDate: ' + meta.createdDate);
+                console.log('fileName: ' + meta.fileName);
+                console.log('fullName: ' + meta.fullName);
+                console.log('id: ' + meta.id);
+                console.log('lastModifiedById: ' + meta.lastModifiedById);
+                console.log('lastModifiedByName: ' + meta.lastModifiedByName);
+                console.log('lastModifiedDate: ' + meta.lastModifiedDate);
+                console.log('manageableState: ' + meta.manageableState);
+                console.log('namespacePrefix: ' + meta.namespacePrefix);
+                console.log('type: ' + meta.type);
+            })*/
+
+            /*
+            //This give the information on the Objects
+            conn.describeGlobal(function (err,res){
+                if (err){return console.error(err)}
+                console.log('No of Objects ' + res)
+                var customObject =[]
+                var standardObject =[]
+                res.sobjects.forEach(function(sobject){
+                    if (sobject.custom){
+                        customObject.push(sobject)
+                    }else{
+                        standardObject.push(sobject)
+                    }
+                })
+                console.log("Standard object : " + standardObject.length)
+                console.log("Custom Object : " + customObject.length)
+            })*/
+
+            /*
+            //This get the fields in the Visualforce Page?
+            conn.tooling.sobject('ApexPage').describe(function (err, meta){
+                if (err){return console.error(err)}
+                console.log("label name : " + meta.label)
+                var i = 1
+                meta.fields.forEach(function(item){
+                    console.log(' *' + i + ' :' + item.label)
+                    i++
+                })
+            })
+            //This get the fields in the ApexTrigger
+            conn.tooling.describe('ApexTrigger', function(err, result){
+                if (err){return console.error(err)}
+                var i = 1
+                result.fields.forEach(function(item){
+                    console.log(i + ':' + item.label)
+                    i++
+                })
+            })*/
+}
+
+
+module.exports = {
+    sObjectDescribe : sObjectDescribe,
+    meta : meta,
+    dofinally: dofinally
+}
