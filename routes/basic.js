c9fb477028f1d5c1566c6b39e91d606caabdefbd
Lance Goh <gohlance@gmail.com>
2019-12-16 14:10:33 +0800

    Again with Welcome Page editing

--- routes/basic.js
+++ routes/basic.js
@@ -20,7 +20,15 @@ module.exports = ({
         return ctx.render('index')
       }else{
         console.log(display_Homepage_Profiles())
-        return ctx.render('welcome',{result_objects: await display_Homepage_Objects(), result_profiles: await display_Homepage_Profiles(), result_layouts: await display_Homepage_Layouts()})
+        return ctx.render('welcome',{
+          result_objects: await display_Homepage_Objects(), 
+          result_profiles: await display_Homepage_Profiles(), 
+          result_layouts: await display_Homepage_Layouts(),
+          result_ApexComponents: await something(),
+          result_apexTriggers: await something(),
+          result_apexPages: await something(),
+          result_recordTypes: await display_Homepage_RecordTypes()
+        })
       }
     })
     .get('about', '/about', (ctx) => {
@@ -34,32 +42,41 @@ module.exports = ({
 
 async function display_Homepage_Objects(){
   const result_object = await pool.query('SELECT objectinfo FROM objects WHERE orgid = $1',[global.orgId])
-  const result_profile = await pool.query("SELECT profile FROM profiles WHERE orgid=$1",[global.orgId])
-  let return_Object
   if (result_object.rowCount > 0 ){
-    return_Object = result_object.rows[0]["objectinfo"]["allObject"].length
+   return return_Object = result_object.rows[0]["objectinfo"]["allObject"].length
   }else{
-    return_Object = 0
+   return return_Object = 0
   }
-  return return_Object.toString()
 }
 async function display_Homepage_Profiles(){
   const result_profile = await pool.query("SELECT profile FROM profiles WHERE orgid=$1",[global.orgId])
-  let return_Object
+
   if (result_profile.rows[0]["profile"].size > 0 ){
-    return_Object = result_profile.rows[0]["profile"].size
+   return return_Object = result_profile.rows[0]["profile"].size
   }else{
-    return_Object = 0
+    return return_Object = 0
   }
-  return return_Object.toString()
 }
 async function display_Homepage_Layouts(){
   const result_profile =  await pool.query("SELECT layout FROM layouts WHERE orgid=$1",[global.orgId]) 
-  let return_Object
+
   if (result_profile.rows[0]["layout"].size > 0 ){
-    return_Object = result_profile.rows[0]["layout"].size
+    return return_Object = result_profile.rows[0]["layout"].size
   }else{
-    return_Object = 0
+    return return_Object = 0
   }
-  return return_Object.toString()
+}
+
+async function display_Homepage_RecordTypes(){
+  const result_profile =  await pool.query("SELECT recordtype FROM recordtypes WHERE orgid=$1",[global.orgId])
+ 
+  if (result_profile.rows[0]["recordtype"].size > 0 ){
+    return return_Object = result_profile.rows[0]["recordtype"].size
+  }else{
+    return return_Object = 0
+  }
+}
+
+async function something(){
+  return 0;
 }
