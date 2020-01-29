4c879677b8dfdc2c3142902dee10920dc278123b
Lance Goh <gohlance@gmail.com>
2020-01-29 20:25:09 +0800

    Adding Config to Session

--- index.js
+++ index.js
@@ -32,7 +32,21 @@ var serve = require('koa-static');
 
 app.keys = ['82a5193f-37e4-4dba-bf9c-750389f80699']
 //Session for App
-app.use(session(app))
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 100000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
+
+app.use(session(CONFIG,app))
 
 app.use(serve('./public'))
 
