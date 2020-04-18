d3c15fbeaf1da6180b178aff18c5b6a0bfe4eeae
Lance Goh <gohlance@gmail.com>
2020-04-18 16:02:20 +0800

    Added Check with Lodash to Backgroundsvc.js - adding a closing bracket

--- svc_background/backgroundsvc.js
+++ svc_background/backgroundsvc.js
@@ -56,7 +56,7 @@ async function start_background_call() {
                         JSON.stringify(objectinfo[0]),
                         JSON.stringify(license),
                         JSON.stringify(orglimit.data), 
-                        JSON.stringify(_.defaultTo(securityrisk.records,0), 
+                        JSON.stringify(_.defaultTo(securityrisk.records,0)), 
                         JSON.stringify(objectinfo[1]), 
                         JSON.stringify(apextrigger), 
                         JSON.stringify(apexpage), 
