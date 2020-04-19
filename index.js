7efee97559df4fe24d30da52dd38318b7274eefe
Lance Goh <gohlance@gmail.com>
2020-04-19 11:56:43 +0800

    UX updated. Fixed Imperfection

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ global.orgId = "288"
 
 //global DB Connection
 const Pool = require('pg-pool')
-
+/*
 global.pool = new Pool({
     user: 'bxhbybpvxuyesk',
     host: 'ec2-54-174-221-35.compute-1.amazonaws.com',
@@ -30,8 +30,8 @@ global.pool = new Pool({
     max: 20, // set pool max size to 20
     min: 4
 }) 
-  
-/*
+  */
+
 //DEV
 global.pool = new Pool({
   user: 'postgres',
@@ -41,7 +41,7 @@ global.pool = new Pool({
   port: 5432,
   max: 20, // set pool max size to 20
   min: 4
-})*/
+})
 
 render(app, {
   root: path.join(__dirname, 'views'),
