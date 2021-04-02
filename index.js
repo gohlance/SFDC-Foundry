bdc308191428aac8bb241935ad2cb7cf97de8a73
Lance <gohlance@gmail.com>
2021-04-02 13:36:34 +0800

    Production Connection String

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = new Koa()
 
 // override koa's undocumented error handler
 app.context.onerror = errorHandler;
-
+/**
 //** Only for Development 
 global.instanceUrl = "https://lgoh-isos2020.my.salesforce.com"
 global.accesscode = "00D2x000000D8Li!AQ8AQCHbGHxT1bZl9RV3KJN8LyKkn5s.Pc1G03uN3p.4AnUCTAVmSGESdWSdM4IO3tD.NrTIwD0NEIX4LEz.62M.x9X9l0Zg"
@@ -30,7 +30,7 @@ global.pool = new Pool({
   max: 20, // set pool max size to 20
   min: 4
 })
-/**
+ */
  //global DB Connection
 const Pool = require('pg-pool')
 
@@ -43,7 +43,7 @@ global.pool = new Pool({
     max: 20, // set pool max size to 20
     min: 4
 })   
- */
+
 render(app, {
   root: path.join(__dirname, 'views'),
   layout: 'layout',
